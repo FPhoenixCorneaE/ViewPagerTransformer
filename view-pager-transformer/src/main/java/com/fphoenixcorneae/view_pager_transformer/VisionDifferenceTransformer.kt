d5f5c4855daff79da7cbe86f325ab384ca4784a9
ÍôÀ¤Öù <wangkz@galanz.com>
2020-04-09 15:19:00 +0800

    feat:视觉差的转换效果;

---
+++ view-pager-transformer/src/main/java/com/fphoenixcorneae/view_pager_transformer/VisionDifferenceTransformer.kt
@@ -0,0 +1,24 @@
+package com.fphoenixcorneae.view_pager_transformer
+
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * @desc 视觉差的转换效果
+ * @date 2020-04-09 15:11
+ */
+class VisionDifferenceTransformer @JvmOverloads constructor(
+        private var speed: Float = 0.5f
+) : AbstractBaseTransformer() {
+
+    override fun onTransform(page: View, position: Float) {
+        when (page) {
+            is ViewGroup -> {
+                (0 until page.childCount)
+                        .asSequence()
+                        .map { page.getChildAt(it) }
+                        .forEach { it.translationX = -page.getWidth() * position * speed }
+            }
+        }
+    }
+}
